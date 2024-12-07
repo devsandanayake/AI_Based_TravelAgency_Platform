290e707064cc4180e1bc5f0dcd58763e6bb974d4
devin <devinsandanayake49@gmail.com>
2024-12-07 17:08:41 +0530

    update

--- LocationServiceAR/geospatial/app/src/main/java/com/google/ar/core/examples/java/common/helpers/DisplayRotationHelper.java
+++ LocationServiceAR/geospatial/app/src/main/java/com/google/ar/core/examples/java/common/helpers/DisplayRotationHelper.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.google.ar.core.examples.java.common.helpers;
 
 import android.app.Activity;
@@ -27,11 +12,7 @@ import android.view.Surface;
 import android.view.WindowManager;
 import com.google.ar.core.Session;
 
-/**
- * Helper to track the display rotations. In particular, the 180 degree rotations are not notified
- * by the onSurfaceChanged() callback, and thus they require listening to the android display
- * events.
- */
+ 
 public final class DisplayRotationHelper implements DisplayListener {
   private boolean viewportChanged;
   private int viewportWidth;
@@ -93,10 +74,7 @@ public final class DisplayRotationHelper implements DisplayListener {
     }
   }
 
-  /**
-   *  Returns the aspect ratio of the GL surface viewport while accounting for the display rotation
-   *  relative to the device camera sensor orientation.
-   */
+ 
   public float getCameraSensorRelativeViewportAspectRatio(String cameraId) {
     float aspectRatio;
     int cameraSensorToDisplayRotation = getCameraSensorToDisplayRotation(cameraId);
